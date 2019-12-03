fd2000debce118d54c5cb5d2c569dde2e98403ad
nithahuwaida <hafizhahuwaida@gmail.com>
2019-12-03 17:43:34 +0700

    update ANTD

---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'antd/dist/antd.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-body">
+      </div>
+    </div>
+  );
+}
+
+export default App;
